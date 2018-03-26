459aef1ac7a1f71d7da160bf3726b2b758b08a2b
g44422 <g44422@L403P04.pdg.esi.heb.site>
2018-03-26 12:07:04 +0200

    V0.1 3.3 Added Controller.java

---
+++ src/g44422/diamond/view/View.java
@@ -0,0 +1,80 @@
+package g44422.diamond.view;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+import g44422.diamond.model;
+
+public class View {
+    
+    Scanner in = new Scanner(System.in);
+    
+    public View(Model game) {
+        this.in;
+        this.game = new Game();
+    }
+    
+    public Explorer askExplorer() {
+        String answer = "";
+        System.out.println("Entrez un pseudonyme pour cet exploratreur:");
+        while(in.nextLine().equalsIgnoreCase("")) {
+            System.out.println("Le pseudonyme ne peut pas être vide");
+            answer = in.nextLine();
+        }
+        return new Explorer(answer);
+    }
+    
+    public boolean isThereNewExplorerToAdd() {
+        System.out.println("Voulez-vous ajouter un nouvel utilisateur? [ OUI / NON ]");
+        String expectedResults[] = {"oui", "o", "non", "n"};
+        ask:
+        while(true) {
+            String answer = in.nextLine();
+            if(Arrays.asList(expectedResults).contains(answer.toLowerCase())) {
+                switch(answer) {
+                    case "oui":
+                    case "o":
+                        return true;
+                    case "non":
+                    case "n":
+                        return false;
+                    default:
+                        System.out.println("Réponse non valide. Entrez une des proposition suivantes : [ OUI / NON ]");
+                }
+            }
+        }
+    }
+    
+    public boolean askExplorerChoiceToContinue(Explorer explorer) {
+        String expectedResults[] = {"oui", "o", "non", "n"};
+        
+        System.out.println(explorer.getPseudonym+", voulez-vous quitter la grotte? [ OUI / NON ]");
+        ask:
+        while(true) {
+            String answer = in.nextLine();
+            if(Arrays.asList(expectedResults).contains(answer.toLowerCase())) {
+                switch(answer) {
+                    case "oui":
+                    case "o":
+                        explorer.takeDecisionToLeave();
+                        return true;
+                    case "non":
+                    case "n":
+                        return false;
+                    default:
+                        System.out.println("Réponse non valide. Entrez une des proposition suivantes : [ OUI / NON ]");
+                }
+            }
+        }
+    }
+    
+    public void displayGame() {
+    }
+    
+    public void displayEnd() {
+        System.out.println("Fin du jeu:\n===========\nClassement:");
+        for(int i=0;i<explorers.size();i++) {
+            System.out.println(explorers.get(i).getPseudonym()+"\t\t"+explorers.get(i).getBag.nbRubies);
+        }
+    }
+}
