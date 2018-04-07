4ad0d3bbad0dbc85eae5fbc1efdbb9983337dbc1
g44422 <g44422@L403P04.pdg.esi.heb.site>
2018-04-07 12:05:33 +0200

    v0.1.1 Added GameException

---
+++ src/g44422/diamond/model/GameException.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author g44422
+ */
+package g44422.diamond.model;
+
+public class GameException extends RuntimeException{
+
+	/**
+	 * Creates a new instance of GameException without detail message.
+	 */
+	public GameException() {}
+
+	/**
+	 * Constructs an instance of GameException with the specified detail message.
+	 * @param msg the detail message.
+	 */
+	public GameException(String msg) {
+		super(msg);
+	}
+}
